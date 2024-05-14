b7fc91604107300f47d92b401ea9e1142e2b09bb
Obedson <115647152+richie997@users.noreply.github.com>
2024-05-15 00:15:30 +0200

    implement the homapage UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
+  const loggedIn = { firstName: "Obed" };
   return (
-    <div>
-         Home
-    </div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import CountUp from "react-countup";
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+  return (
+    <div className="w-full">
+      <CountUp decimals={2} decimal="," prefix="$" end={amount} />
+    </div>
+  );
+};
+
+export default AnimatedCounter;
