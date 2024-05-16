4f8abde665909f1edb7e17b14629bfb291ee29ff
Obedson <115647152+richie997@users.noreply.github.com>
2024-05-16 20:00:14 +0200

    modifying and implementing the sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,27 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Obed", lastName: "Malonda" };
   return (
-     <main>
-         SIDEBAR
-         {children}
-     </main>
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
+  
+    </main>
   );
 }
